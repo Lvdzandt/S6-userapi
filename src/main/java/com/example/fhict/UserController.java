0d00c451ddeaaba461bafd128e6b724f0351a38d
Lvdzandt <lucvdzandt@gmail.com>
2022-06-09 11:30:39 +0200

    added kafka

--- src/main/java/com/example/fhict/UserController.java
+++ src/main/java/com/example/fhict/UserController.java
@@ -15,23 +15,29 @@ public class UserController {
         this.repo = repo;
     }
 
-    @CrossOrigin(origins = "http://localhost:8083")
+    @org.springframework.kafka.annotation.KafkaListener(topics = "topic_1",groupId = "groupId")
+    void listener(String data){
+        System.out.println("listener: " + data);
+    }
+
+    @CrossOrigin(origins = {"http://localhost:8083","http://localhost:8080"})
     @PostMapping("/")
     public User postUser(@RequestBody User user){
         return repo.save(user);
     }
 
-    @CrossOrigin(origins = {"http://localhost:8083","http://localhost:9080"})
+    @CrossOrigin(origins = {"http://localhost:8083","http://localhost:9080","http://localhost:8080"})
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable(value = "id") int id){
         User result = repo.findUserById(id);
         return ResponseEntity.ok(result);
     }
 
-    @CrossOrigin(origins = {"http://localhost:8083","http://localhost:9080"})
+    @CrossOrigin(origins = {"http://localhost:8083","http://localhost:9080","http://localhost:8080"})
     @DeleteMapping({"/{id}"})
-    public ResponseEntity<User> deleteUserById(@PathVariable(value = "id")int id){
-        User deleted = repo.deleteUserById(id);
-        return ResponseEntity.ok(deleted);
+    public ResponseEntity<String> deleteUserById(@PathVariable(value = "id")int id){
+        User deleted = repo.findUserById(id);
+        repo.delete(deleted);
+        return ResponseEntity.ok("deleted");
     }
 }
